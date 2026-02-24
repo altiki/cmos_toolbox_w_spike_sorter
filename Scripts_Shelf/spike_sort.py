4dd5f5a0a8c003273e5bff9fab6a63af7cfc41ab
altiki <87695887+altiki@users.noreply.github.com>
2026-02-24 16:04:46 +0100

    Refactor paths in spike_sort.py
    Updated recording and output paths to placeholders.

--- Scripts_Shelf/spike_sort.py
+++ Scripts_Shelf/spike_sort.py
@@ -1,10 +1,6 @@
 #  Import libraries
 import sys
-
 sys.path.append('/home/kvulic/Vulic/cmos_toolbox_w_spike_sorter/')
-
-
-
 import os
 from src.utils.logger_functions import console
 from src.utils.metadata_functions import load_metadata_as_dataframe
@@ -12,16 +8,9 @@ from src.cmos_extractor.Extractor_Class_KVedit import SortingExtractor
 
 
 
-# Define variables
-#RECORDINGS_PATH = f"/itet-stor/kvulic/neuronies/single_neurons/3_Student_Projects/Amelie/Raw_data/Chip{chip_id}/Chip{chip_id}_DIV42/"
-#OUTPUT_PATH = f"/itet-stor/kvulic/neuronies/single_neurons/3_Student_Projects/Amelie/Processed_data/Chip{chip_id}/Chip{chip_id}_DIV42/"
 
-#RECORDINGS_PATH = f"/itet-stor/kvulic/neuronies/gbm_project/3_Student_Projects/Luc/Raw_Data/Raw_Traces/Primary_MEA_3/"
-#RECORDINGS_PATH = f"/itet-stor/kvulic/neuronies/information_capacity/1_Raw_Data/Electrophysiology/Recordings/DIV30/"
-#OUTPUT_PATH = "/itet-stor/kvulic/neuronies/gbm_project/1_Subprojects/Information_Capacity/Recordings/Processed_Data/"
-#OUTPUT_PATH = "/itet-stor/kvulic/neuronies/information_capacity/1_Raw_Data/Electrophysiology/Processed_Data/DIV30/"
-RECORDINGS_PATH = "/itet-stor/kvulic/neuronies/Karan/Ephys_recordings/For_Kate/Test/"
-OUTPUT_PATH = "/itet-stor/kvulic/neuronies/Karan/Ephys_recordings/For_Kate/Processed_Data/"
+RECORDINGS_PATH = "..."
+OUTPUT_PATH = "..."
 if __name__ == "__main__":
     # Load (or create) metadata
     metadata = load_metadata_as_dataframe(file_path=RECORDINGS_PATH)
@@ -31,7 +20,6 @@ if __name__ == "__main__":
             console.info("File was already processed.")
 
         else:
-        #elif "N3" in filename:
             try: 
                 console.info(f"Processing file {filename}...")
                 extractor = SortingExtractor(
